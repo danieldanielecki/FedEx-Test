2d07063e1d0e66340d3208f284d947e20215ff06
Daniel Danielecki <daniel.danielecki@mail.com>
2020-03-08 23:47:46 +0100

    Remove: custom webpack

--- webpack.browser.config.js
+++
@@ -1,8 +0,0 @@
-/* Custom webpack browser properties. */
-
-const Dotenv = require('dotenv-webpack');
-const webpackConfig = {
-  plugins: [new Dotenv()]
-};
-
-module.exports = webpackConfig; // Export all custom Webpack configs.
