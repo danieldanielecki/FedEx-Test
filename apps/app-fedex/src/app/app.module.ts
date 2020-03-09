d1376342cea014a9ea40789820329bf3e5d690b1
Daniel Danielecki <daniel.danielecki@mail.com>
2020-03-09 02:45:45 +0100

    Change: application architecture

--- apps/app-fedex/src/app/app.module.ts
+++ apps/app-fedex/src/app/app.module.ts
@@ -1,44 +1,12 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import {
-  ErrorStateMatcher,
-  ShowOnDirtyErrorStateMatcher
-} from '@angular/material/core';
-import { FlexLayoutModule } from '@angular/flex-layout';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
-import { HTTP_INTERCEPTORS } from '@angular/common/http';
-import { HttpErrorInterceptor } from './http-error.interceptor';
-import { MatButtonModule } from '@angular/material/button';
-import { MatCardModule } from '@angular/material/card';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatInputModule } from '@angular/material/input';
-import { RegisterService } from './register.service';
+import { CoreModule } from '@fedex-test/core';
+import { HomeModule } from '@fedex-test/home';
+import { NgModule } from '@angular/core';
+import { SharedModule } from '@fedex-test/shared';
 
 @NgModule({
+  bootstrap: [AppComponent],
   declarations: [AppComponent],
-  imports: [
-    BrowserModule,
-    BrowserAnimationsModule,
-    FlexLayoutModule,
-    FormsModule,
-    HttpClientModule,
-    ReactiveFormsModule,
-    MatButtonModule,
-    MatCardModule,
-    MatFormFieldModule,
-    MatInputModule
-  ],
-  providers: [
-    // Display errors instantly.
-    { provide: ErrorStateMatcher, useClass: ShowOnDirtyErrorStateMatcher },
-    // HTTP Interceptors.
-    { provide: HTTP_INTERCEPTORS, useClass: HttpErrorInterceptor, multi: true },
-    RegisterService
-  ],
-  bootstrap: [AppComponent]
+  imports: [CoreModule, HomeModule, SharedModule]
 })
 export class AppModule {}
